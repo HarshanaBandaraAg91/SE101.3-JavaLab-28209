f67b7a30f7e382e614e31aef5d688860ab1420ce
HarshanaBandaraAg91 <139755295+HarshanaBandaraAg91@users.noreply.github.com>
2023-07-29 15:02:09 +0530

    Add files via upload

---
+++ Lab03-Pract03/EncapsulationQ02/NewQuestion01Replace.java
@@ -0,0 +1,12 @@
+
+package com.mycompany.newquestion01replace;
+
+public class NewQuestion01Replace 
+{
+
+    public static void main(String[] args) 
+    {
+        Encapsulation2 e1=new Encapsulation2("Harshana", 21, 90000.00f);
+        e1.display();
+    }
+}
